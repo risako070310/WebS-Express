e6846c791f84ca7f80419aea6dc546be344bba04
risako070310 <risako070310@gmail.com>
2021-08-19 05:17:06 +0000

    finish textbook #004

--- app.rb
+++ app.rb
@@ -6,14 +6,18 @@ require 'json'
 require 'net/http'
 
 get '/' do
+    erb :form
+end
+
+get '/list' do
     uri = URI("http://express.heartrails.com/api/json")
     uri.query = URI.encode_www_form({
         method: "getStations",
-        x: 139.73,
-        y: 35.64
+        x: params[:x],
+        y: params[:y]
     })
     res = Net::HTTP.get_response(uri)
     json = JSON.parse(res.body)
     @stations = json["response"]["station"]
-    erb :index
+    erb :list
 end
